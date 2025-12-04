4680247262249c10534a101314afb903df8800e5
loicsikidi <loic.sikidi@gmail.com>
2025-12-04 13:10:51 +0100

    feat(gotest): add short flag

--- nix/toolbox/gotest.nix
+++ nix/toolbox/gotest.nix
@@ -1,11 +1,32 @@
-{ pkgs }:
-
+{pkgs}:
 pkgs.writeShellApplication {
   name = "gotest";
-  runtimeInputs = [ pkgs.go ];
+  runtimeInputs = [pkgs.go];
   text = ''
+    SHORT=false
+    while [[ $# -gt 0 ]]; do
+      case $1 in
+        --short)
+          SHORT=true
+          shift
+          ;;
+        *)
+          echo "Unknown option: $1"
+          echo "Usage: gotest [--short]"
+          exit 1
+          ;;
+      esac
+    done
+
     mapfile -t paths < <(go list ./... | grep -vE '/proto') # exclude generated code
-    if ! go test -short=true -count=1 -failfast -covermode=count -coverprofile=coverage.out -v "''${paths[@]}"; then
+
+    if [ "$SHORT" = true ]; then
+      SHORT_FLAG="-short=true"
+    else
+      SHORT_FLAG="-short=false"
+    fi
+
+    if ! go test $SHORT_FLAG -count=1 -failfast -covermode=count -coverprofile=coverage.out -v "''${paths[@]}"; then
       echo "tests failed ⛔"
       exit 1
     fi
