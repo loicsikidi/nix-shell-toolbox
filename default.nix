2096c9cf1477a40f06322a3fcf1df73d816c3251
loicsikidi <loic.sikidi@gmail.com>
2025-12-07 18:35:34 +0100

    fix(toobox): clean code

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
-{ pkgs ? import <nixpkgs> { }, toolboxConfig ? { }, hooksConfig ? { } }:
-
-let
-  toolbox = import ./nix/toolbox { inherit pkgs; enabled = toolboxConfig; };
-  pre-commit = import ./nix/pre-commit.nix { inherit pkgs hooksConfig; };
-in
 {
+  pkgs ? import <nixpkgs> {},
+  toolboxConfig ? {},
+  hooksConfig ? {},
+}: let
+  toolbox = import ./nix/toolbox {inherit pkgs toolboxConfig;};
+  pre-commit = import ./nix/pre-commit.nix {inherit pkgs hooksConfig;};
+in {
   # All packages (toolbox + pre-commit)
   # Use this for buildInputs to get everything
   packages = toolbox ++ pre-commit.enabledPackages;
--- nix/toolbox/default.nix
+++ nix/toolbox/default.nix
@@ -1,26 +1,26 @@
-{ pkgs ? import <nixpkgs> { }, enabled ? { }, toolboxConfig ? enabled }:
-
-let
+{
+  pkgs ? import <nixpkgs> {},
+  toolboxConfig ? {},
+}: let
   # Default configuration - all tools enabled by default
-  defaultEnabled = {
+  defaultConfig = {
     gotest = true;
     lint = true;
     check-workflows = true;
   };
 
   # Merge user config with defaults
-  config = defaultEnabled // toolboxConfig;
+  config = defaultConfig // toolboxConfig;
 
   # All available tools
   allTools = {
-    gotest = import ./gotest.nix { inherit pkgs; };
-    lint = import ./lint.nix { inherit pkgs; };
-    check-workflows = import ./zizmor.nix { inherit pkgs; };
+    gotest = import ./gotest.nix {inherit pkgs;};
+    lint = import ./lint.nix {inherit pkgs;};
+    check-workflows = import ./zizmor.nix {inherit pkgs;};
   };
 
   # Filter tools based on enabled config
   enabledTools = pkgs.lib.filterAttrs (name: _: config.${name} or false) allTools;
-
 in
-# Return list of enabled derivations for buildInputs
-pkgs.lib.attrValues enabledTools
+  # Return list of enabled derivations for buildInputs
+  pkgs.lib.attrValues enabledTools
