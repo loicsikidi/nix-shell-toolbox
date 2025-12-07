fa464d50460c7c27366464f38ece41d4fc4bf516
loicsikidi <loic.sikidi@gmail.com>
2025-12-07 18:24:50 +0100

    feat(gotest): support --race flag

--- nix/toolbox/gotest.nix
+++ nix/toolbox/gotest.nix
@@ -4,15 +4,20 @@ pkgs.writeShellApplication {
   runtimeInputs = [pkgs.go];
   text = ''
     SHORT=false
+    RACE=false
     while [[ $# -gt 0 ]]; do
       case $1 in
         --short)
           SHORT=true
           shift
           ;;
+        --race)
+          RACE=true
+          shift
+          ;;
         *)
           echo "Unknown option: $1"
-          echo "Usage: gotest [--short]"
+          echo "Usage: gotest [--short] [--race]"
           exit 1
           ;;
       esac
@@ -26,7 +31,13 @@ pkgs.writeShellApplication {
       SHORT_FLAG="-short=false"
     fi
 
-    if ! go test $SHORT_FLAG -count=1 -failfast -covermode=count -coverprofile=coverage.out -v "''${paths[@]}"; then
+    if [ "$RACE" = true ]; then
+      RACE_FLAG="-race"
+    else
+      RACE_FLAG=""
+    fi
+
+    if ! go test $SHORT_FLAG $RACE_FLAG -count=1 -failfast -covermode=count -coverprofile=coverage.out -v "''${paths[@]}"; then
       echo "tests failed ⛔"
       exit 1
     fi
