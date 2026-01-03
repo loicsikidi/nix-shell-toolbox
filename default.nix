8fac2fe3c1569f9240c6e131d6a0a34b708d4d2b
loicsikidi <loic.sikidi@gmail.com>
2026-01-03 07:48:48 +0100

    feat: add delve as a common pkg

--- default.nix
+++ default.nix
@@ -5,10 +5,16 @@
 }: let
   toolbox = import ./nix/toolbox {inherit pkgs toolboxConfig;};
   pre-commit = import ./nix/pre-commit.nix {inherit pkgs hooksConfig;};
+  commonPackages =
+    toolbox
+    ++ pre-commit.enabledPackages
+    ++ [
+      pkgs.delve
+    ];
 in {
   # All packages (toolbox + pre-commit)
   # Use this for buildInputs to get everything
-  packages = toolbox ++ pre-commit.enabledPackages;
+  packages = commonPackages;
 
   # Pre-commit shellHook
   # Use this in mkShell's shellHook
